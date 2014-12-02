a6325396da12d8c61977efcf9f548f5a9e9e72b3
Fumio Furukawa <fumio@straight-spirits.com>
2014-12-02 22:14:36 +0900

    Update facades.

--- config/app.php
+++ config/app.php
@@ -206,6 +206,9 @@ return [
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
 
+		'Form'      => 'Illuminate\Html\FormFacade',
+		'Html'      => 'Illuminate\Html\HtmlFacade',
+
 	],
 
 ];
