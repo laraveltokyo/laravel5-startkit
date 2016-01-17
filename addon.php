8ea1f4343bb2be0cb16b5b0a9c65c7ec8889afab
Fumio Furukawa <fumio@jumilla.me>
2016-01-18 06:56:06 +0900

    Squashed 'addons/debug/' changes from e7931f6..1536906
    1536906 Update.
    164e7d3 Update base controller.
    65888bf Disable routes if debug mode.
    4f14aa4 Use namespace alias.
    c1a079c Add composer.json.
    341b789 First structure.
    REVERT: e7931f6 Update extension version.
    REVERT: 4f50f0b Merge commit '30213c54e0fa2eb145c37fad64d23e02fec62d95'
    REVERT: 30213c5 Squashed 'addons/debug/' changes from 65888bf..164e7d3
    REVERT: 34eb0c6 Merge commit '86974b023398a44600b91b600148c03e91548980'
    REVERT: 86974b0 Squashed 'addons/auth/' changes from b04473e..882d008
    REVERT: 7798ef6 Add User.php
    REVERT: 936705b Update framework tables.
    REVERT: fc0aa4b Upgrade to Laravel 5.2
    REVERT: 158bd25 Update languages.
    REVERT: 912cdb0 Squashed 'addons/auth/' changes from aca72e4..b04473e
    REVERT: 2708af6 Merge commit '912cdb06c7bdb8057146f3e4640fda229514e3d1' into develop
    REVERT: 77c3089 Squashed 'addons/debug/' changes from 341b789..65888bf
    REVERT: e79c3ba Merge commit '77c3089ce2a0ac03d2bd061c215ba24d21b810ec' into develop
    REVERT: 7756470 Merge commit '7e607f75e2a890de73ba6dbed482143d084c81dc' into develop
    REVERT: 7e607f7 Squashed 'addons/generator/' changes from 00de13f..7a74950
    REVERT: 7d75554 Add section of 'pre-update-cmd'.
    REVERT: 8ee5680 Add 46 languages files.
    REVERT: 2bd933b Upgrade laravel-plus/extension.
    REVERT: baa10d6 Squashed 'addons/generator/' changes from 7d8c929..00de13f
    REVERT: 7dbb20b Merge commit 'baa10d6a897c53b3c116f76b5f7577d7854d0e5c'
    REVERT: 7b6400d Squashed 'addons/generator/' changes from b54b471..7d8c929
    REVERT: e58af3c Merge commit '7b6400d3174ab70cdd193543962c1b457da1cc34'
    REVERT: 6677a03 Update readme.
    REVERT: 2562ec6 Move.
    REVERT: f7b3e4b Squashed 'addons/generator/' changes from 6902c01..b54b471
    REVERT: 20d9e4c Merge commit 'f7b3e4b058a9b51e13f53a269929fc8ca63ccaa8' into develop
    REVERT: 81cdecc Change phpunit coverage option.
    REVERT: 46441e6 Squashed 'addons/auth/' content from commit aca72e4
    REVERT: 5b50a5f Merge commit '46441e6517d1d498e49811574186093972a01732' as 'addons/auth'
    REVERT: 1bc40ce Squashed 'addons/debug/' content from commit 341b789
    REVERT: 7e3f3f5 Merge commit '1bc40cee57aa5e4ab9f2355b227c8efb7b83534d' as 'addons/debug'
    REVERT: b8c74cf WIP
    REVERT: 9b962fb Squashed 'addons/generator/' content from commit 6902c01
    REVERT: befd208 Merge commit '9b962fbf0d908ec4a9968e44c9999c1098c113aa' as 'addons/generator'
    REVERT: 613fd5a Add package `guzzlehttp/guzzle`
    REVERT: ce904f0 Update readme.
    REVERT: 95f5008 Add validation rule 'json' message. Update composer.json
    REVERT: 7ab0963 Upgrade to v5.1.11
    REVERT: 6a5def0 Update.
    REVERT: 7941ce0 Upgrade to Laravel 5.1.4.
    REVERT: 11df151 Upgraded to Laravel 5.1
    REVERT: 7f8e81a [WIP] Upgrade to Laravel 5.1
    REVERT: 561311a Add view 'emails.password'.
    REVERT: ac1a794 Fix.
    REVERT: 26c5b0b Move middleware for auth.
    REVERT: 8364edf Add default migrations 'sessions' and 'jobs'.
    REVERT: bfe1036 Update addon 'auth'.
    REVERT: 71e9d81 Add script on 'composer create-project'.
    REVERT: 6922221 Brush up.
    REVERT: 0586520 Update base laravel/laravel=5.0.22
    REVERT: d6d58a8 Add facades.
    REVERT: e67d96b Add package 'laracasts/generators'
    REVERT: 666546e Update.
    REVERT: ab9e145 Updat composer.json
    REVERT: 6b3616f Upgrade to Laravel 5.0.1.
    REVERT: ac5ecfb Update composer.json.
    REVERT: 69d5423 Tweak.
    REVERT: 1094609 Upgrade to Laravel5.0-dev-20141217
    REVERT: 62d82be Add facades.
    REVERT: 0313bee Upgrade to Laravel 5.0-dev-20141214
    REVERT: ac8f0ba Update readme.md
    REVERT: a632539 Update facades.
    REVERT: 575acbe Update readme.
    REVERT: de34ffa Upgrade to Laravel5.0-dev-20141201
    REVERT: 72d7ab8 Add config for production.
    REVERT: 8d6f672 Add Form/Html facades.
    REVERT: b2fb597 Update.
    REVERT: 73fe3d9 Upgrade to Laravel5.0-20141127.
    REVERT: 5db2697 Update laravel-extension.
    REVERT: f223177 Add .env for local
    REVERT: 7dfcbf0 Laravel5.0 (2014/11/18)
    
    git-subtree-dir: addons/debug
    git-subtree-split: 15369067fa4a19d6f60de729829c8942b80ab966

---
+++ addon.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Debug;
+
+return [
+    'version' => 5,
+    'namespace' => __NAMESPACE__,
+    'directories' => [
+        'classes',
+    ],
+    'paths' => [
+        'assets' => 'resources\/assets',
+        'lang' => 'resources\/lang',
+        'specs' => 'resources\/specs',
+        'views' => 'resources\/views',
+        'tests' => 'tests',
+    ],
+    'providers' => [
+        Providers\AddonServiceProvider::class,
+        Providers\RouteServiceProvider::class,
+    ],
+    'console' => [
+        'commands' => [],
+    ],
+    'http' => [
+        'middlewares' => [],
+        'route_middlewares' => [],
+    ],
+    'includes_global_aliases' => true,
+    'aliases' => [],
+];
