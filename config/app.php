20caf6433b39558f580fd228b8eb85c5c82157de
Fumio Furukawa <fumio@jumilla.me>
2016-08-28 23:27:27 +0900

    Add commented line.

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
+        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\DatabaseServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
